3363d12eb9e4e6454ea809a73ff9477d8aea6ef5
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-10 12:14:17 +0200

    displaying data in a component

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,10 +1,29 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur-image" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    );
+    // for (const pokemon of pokemonList) {}
+    const pokemon = pokemonList[0];
+    // const pokemon = pokemonList[1];
+    if (pokemon.imgSrc) {
+        return (
+            <figure>
+                <img
+                    src={pokemon.imgSrc} alt={pokemon.name}
+                />
+                <figcaption>{pokemon.name}</figcaption>
+            </figure>
+        );
+    }
+    return <p>???</p>;
+
 }
 
 export default PokemonCard;
