8c5434bdc792ae4fa54789efccc559774fbb9ee0
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-11 12:17:09 +0200

    Adding conditional rendering to the buttons

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -18,19 +18,45 @@ function App() {
   console.log(pokemonIndex);
 
   const handleClickOnPreviousButton = () => {
+    if (pokemonIndex[0] > 0) {
+      pokemonIndex[1](pokemonIndex[0] - 1); // pokemonIndex[1] is the setter function
+    }
   }
-  
+
   const handleClickOnNextButton = () => {
+    if (pokemonIndex[0] < pokemonList.length - 1) {
+      pokemonIndex[1](pokemonIndex[0] + 1);
+    }
   }
-  
+
   return (
-    <>
-      <PokemonCard pokemon={pokemonList[0]} />
-      <div className="buttons">
-        <button onClick={handleClickOnPreviousButton}>Previous</button>
-        <button onClick={handleClickOnNextButton}>Next</button>
-      </div>
-    </>
+    pokemonIndex[0] === 0 ? (
+      <>
+        <PokemonCard pokemon={pokemonList[0]} />
+        <div className="buttons">
+          {/* <button onClick={handleClickOnPreviousButton}>Previous</button> */}
+          <button onClick={handleClickOnNextButton}>Next</button>
+        </div>
+      </>
+    ) : (
+      pokemonIndex[0] === pokemonList.length - 1 ? (
+        <>
+          <PokemonCard pokemon={pokemonList[1]} />
+          <div className="buttons">
+            <button onClick={handleClickOnPreviousButton}>Previous</button>
+            {/* <button onClick={handleClickOnNextButton}>Next</button> */}
+          </div>
+        </>
+      ) : (
+        <>
+          <PokemonCard pokemon={pokemonList[1]} />
+          <div className="buttons">
+            <button onClick={handleClickOnPreviousButton}>Previous</button>
+            <button onClick={handleClickOnNextButton}>Next</button>
+          </div>
+        </>
+      )
+    )
   );
 }
 
